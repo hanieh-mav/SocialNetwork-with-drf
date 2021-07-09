d929704a3d9f26e1e0ca5d961a01ba7dd5c6bf84
hanieh <h.mehdiabadi@gmail.com>
2021-07-09 18:07:06 +0430

    add feature

--- accounts/forms.py
+++ accounts/forms.py
@@ -8,3 +8,23 @@ class RegisterUserForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ('username','email','password1','password2')
+
+
+
+    def clean_username(self):
+        username = self.cleaned_data['username']
+        if username:
+            if User.objects.filter(username=username).exists():
+                raise forms.ValidationError('username exists')
+        return username
+
+
+
+    def clean_password(self):
+        password = self.cleaned_data['password1']
+        if not any (ch.isdigit() for ch in password):
+            raise forms.ValidationError('Password must have numbers')
+        if not any (ch.isalpha() for ch in password):
+            raise forms.ValidationError('Password must have alphabet')
+        return password
+
